9b23d3e084ad42c70c91df15d1d31e198cbe7fa0
vincent <495285586@qq.com>
2018-08-17 22:28:00 +0800

    up

---
+++ Python/Python 解释器/interpreter_test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2018/8/17 22:15
+
+@author: vincent
+"""
+
+import time
+
+while 1:
+    print('6')
+    time.sleep(3)
---
+++ Python/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2018/8/17 21:34
+
+@author: vincent
+"""
